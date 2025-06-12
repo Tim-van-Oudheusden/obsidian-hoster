150d62352f6c93ed328f0e733fa18c04484387d3
Tim-van-Oudheusden <timvanoudheusden@protonmail.com>
2025-06-12 17:10:44 +0200

    fix(fonts): page title fonts now downloadable to local

--- src/util/theme.ts
+++ src/util/theme.ts
@@ -107,6 +107,13 @@ export interface GoogleFontFile {
   extension: string
 }
 
+const fontMimeMap: Record<string, string> = {
+  truetype: "ttf",
+  woff: "woff",
+  woff2: "woff2",
+  opentype: "otf",
+}
+
 export async function processGoogleFonts(
   stylesheet: string,
   baseUrl: string,
@@ -114,21 +121,23 @@ export async function processGoogleFonts(
   processedStylesheet: string
   fontFiles: GoogleFontFile[]
 }> {
-  const fontSourceRegex = /url\((https:\/\/fonts.gstatic.com\/s\/[^)]+\.(woff2|ttf))\)/g
-  const fontFiles: GoogleFontFile[] = []
-  let processedStylesheet = stylesheet
+  const fontSourceRegex = /url\((https:\/\/fonts.gstatic.com\/.+(?:\/|(?:kit=))(.+?)[.&].+?)\)\sformat\('(\w+?)'\);/g
+  const fontFiles: GoogleFontFile[] = [];
+  let processedStylesheet = stylesheet;
 
-  let match
+  let match;
+
   while ((match = fontSourceRegex.exec(stylesheet)) !== null) {
-    const url = match[1]
-    const [filename, extension] = url.split("/").pop()!.split(".")
-    const staticUrl = `https://${baseUrl}/static/fonts/${filename}.${extension}`
+    const url = match[1];
+    const filename = match[2];
+    const extension = fontMimeMap[match[3].toLowerCase()];
+    const staticUrl = `https://${baseUrl}/static/fonts/${filename}.${extension}`;
 
-    processedStylesheet = processedStylesheet.replace(url, staticUrl)
-    fontFiles.push({ url, filename, extension })
+    processedStylesheet = processedStylesheet.replace(url, staticUrl);
+    fontFiles.push({ url, filename, extension });
   }
 
-  return { processedStylesheet, fontFiles }
+  return { processedStylesheet, fontFiles };
 }
 
 export function joinStyles(theme: Theme, ...stylesheet: string[]) {
