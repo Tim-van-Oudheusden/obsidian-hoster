39e479d840d9596c5738e35700390ae9d3b74306
Tim-van-Oudheusden <timvanoudheusden@protonmail.com>
2025-06-12 17:00:19 +0200

    fix(callout): Grid-based callout collapsible animation

--- src/components/scripts/callout.inline.ts
+++ src/components/scripts/callout.inline.ts
@@ -1,41 +1,39 @@
 function toggleCallout(this: HTMLElement) {
-  const outerBlock = this.parentElement!
-  outerBlock.classList.toggle("is-collapsed")
-  const collapsed = outerBlock.classList.contains("is-collapsed")
-  const height = collapsed ? this.scrollHeight : outerBlock.scrollHeight
-  outerBlock.style.maxHeight = height + "px"
+  const outerBlock = this.parentElement!;
 
-  // walk and adjust height of all parents
-  let current = outerBlock
-  let parent = outerBlock.parentElement
-  while (parent) {
-    if (!parent.classList.contains("callout")) {
-      return
-    }
+  outerBlock.classList.toggle("is-collapsed");
 
-    const collapsed = parent.classList.contains("is-collapsed")
-    const height = collapsed ? parent.scrollHeight : parent.scrollHeight + current.scrollHeight
-    parent.style.maxHeight = height + "px"
+  const content = outerBlock.getElementsByClassName("callout-content")[0] as HTMLElement;
 
-    current = parent
-    parent = parent.parentElement
+  if (!content) {
+    return;
   }
+
+  const collapsed = outerBlock.classList.contains("is-collapsed");
+
+  content.style.gridTemplateRows = collapsed ? "0fr" : "1fr";
 }
 
 function setupCallout() {
   const collapsible = document.getElementsByClassName(
     `callout is-collapsible`,
-  ) as HTMLCollectionOf<HTMLElement>
+  ) as HTMLCollectionOf<HTMLElement>;
+
   for (const div of collapsible) {
-    const title = div.firstElementChild
-    if (!title) continue
+    const title = div.getElementsByClassName("callout-title")[0] as HTMLElement;
+    const content = div.getElementsByClassName("callout-content")[0] as HTMLElement;
+    
+    if (!title || !content) {
+      continue;
+    }
 
-    title.addEventListener("click", toggleCallout)
-    window.addCleanup(() => title.removeEventListener("click", toggleCallout))
+    title.addEventListener("click", toggleCallout);
+    
+    window.addCleanup(() => title.removeEventListener("click", toggleCallout));
 
-    const collapsed = div.classList.contains("is-collapsed")
-    const height = collapsed ? title.scrollHeight : div.scrollHeight
-    div.style.maxHeight = height + "px"
+    const collapsed = div.classList.contains("is-collapsed");
+
+    content.style.gridTemplateRows = collapsed ? "0fr" : "1fr";
   }
 }
 
