42dbe0539d35b31e6a6f5cc6c26e0792ba00e9eb
Tim-van-Oudheusden <timvanoudheusden@protonmail.com>
2025-07-02 19:04:35 +0200

    fix: pagetitle size due to reader mode addition

--- website/components/PageTitle.tsx
+++ website/components/PageTitle.tsx
@@ -7,15 +7,15 @@ const PageTitle: WebsiteComponent = ({ fileData, cfg, displayClass }: WebsiteCom
   const title = cfg?.pageTitle ?? i18n(cfg.locale).propertyDefaults.title
   const baseDir = pathToRoot(fileData.slug!)
   return (
-    <h3 class={classNames(displayClass, "page-title")}>
+    <h2 class={classNames(displayClass, "page-title")}>
       <a href={baseDir}>{title}</a>
-    </h3>
+    </h2>
   )
 }
 
 PageTitle.css = `
 .page-title {
-  font-size: 1.75rem;
+  font-size: 1.50rem;
   margin: 0;
   font-family: var(--titleFont);
 }
